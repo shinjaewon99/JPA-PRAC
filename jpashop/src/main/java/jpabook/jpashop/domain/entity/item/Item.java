46c9615c32a454df1e4191b811aab1317a17ab95
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-16 21:49:40 +0900

    refactor : Item 비즈니스 로직 구현

--- jpashop/src/main/java/jpabook/jpashop/domain/entity/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/entity/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.entity.item;
 
 
 import jpabook.jpashop.domain.entity.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,6 +32,29 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //== 비즈니스 로직 ==//
+    /*
+    stock 증가
+    */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+
+    /*
+    stock 감소
+    */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+
+        // 남아있는 재고가 0이하면 안됨
+        if(restStock < 0 ){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 
 }
