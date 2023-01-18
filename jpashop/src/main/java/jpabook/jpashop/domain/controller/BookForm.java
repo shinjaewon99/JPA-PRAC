2c94ab8cbc61c3ebb1f69bad49347c811eb23f3e
shinjaewon99 <tlswodnjs99@naver.com>
2023-01-18 23:54:31 +0900

    feat : Item 등록, 조회 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/controller/BookForm.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain.controller;
+
+import jpabook.jpashop.domain.entity.item.Book;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+
+
+/*     최대한 set 호출을 Controller에서 하지않고, static 클래스를 만들어서 해줘야 좋다.
+public static Book getBook(String author, String isbn, String name, int price, int quantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(quantity);
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+        return book;
+    }
+    */
+}
